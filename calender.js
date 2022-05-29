d3e1e233bef6c6f55fd3cef63232a3976f9328d6
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-05-29 15:51:00 -0400

    added first two time blocks

--- calender.js
+++ calender.js
@@ -1,6 +1,17 @@
 
-
 var today = moment();
 $(".currentdate").append(today.format("dddd, MMMM Do"));
 
-    
+var timeSlot = $(".hour");
+var now = parseInt(moment().format("H"));
+
+$.each(timeSlot, function (i, hour) {
+  var hourId = parseInt($(this).attr("id"));
+  if (hourId === now) {
+    $(this).next().addClass("present");
+  } else if (hourId < now) {
+    $(this).next().addClass("past");
+  } else if (hourId > now) {
+    $(this).next().addClass("future");
+  }
+});
